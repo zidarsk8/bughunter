6f0a9bd4c159f930546ca10d1bd986d6c073399a
Miha Zidar <zidarsk8@gmail.com>
2019-02-28 16:49:53 +0100

    Add fake 3D effect to falling obstacles

--- src/main.py
+++ src/main.py
@@ -1,20 +1,42 @@
 import sys, pygame
+import random
 
 pygame.init()
 
-size = width, height = 1200, 1000
+SIZE = WIDTH, HEIGHT = 1200, 1000
 speed = [1, 1]
-black = 0, 0, 0
+BLACK = 0, 0, 0
+PINK = 200, 111, 150
 
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(SIZE)
+
+clock = pygame.time.Clock()
 
 
 class Obstacle(pygame.sprite.Sprite):
-    def __init__(self, color, width, height):
+    def __init__(self, color, width, height, x=None, y=None):
         super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
+        self._image = pygame.Surface([width, height])
+        self._image.fill(color)
+        self.x = x or int(random.randint(0, 5) * (WIDTH / 5))
+        self.y = y or random.randint(0, HEIGHT)
+        self.set_position()
+
+    def set_position(self):
+        factor = self.y / HEIGHT
+        self.image = pygame.transform.scale(
+            self._image,
+            (
+                int(self._image.get_width() * factor),
+                int(self._image.get_height() * factor),
+            ),
+        )
+
+        translate_x = self.x * factor + WIDTH * (1 - factor) / 2
+        translate_y = self.y
+
         self.rect = self.image.get_rect()
+        self.rect.center = (translate_x, translate_y)
 
 
 class Player(pygame.sprite.Sprite):
@@ -22,14 +44,29 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = pygame.image.load("src/ball_noun_001_01090.jpg")
         self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH // 2, HEIGHT - self.image.get_height() // 2 - 50)
 
 
 def init_obstacles(n):
-    obstacle_group = pygame.sprite.Group()
+    obstacles = pygame.sprite.Group()
     for i in range(n):
-        obstacle_group.add(Obstacle((0, 0, 0), 20, 20))
-    return obstacle_group
+        obstacles.add(Obstacle((52, 220, 60), 40, 40))
+    return obstacles
 
+
+def tick_obstacles(delta_time, obstacles, obstacle_count, bottom_border):
+    collisions = pygame.sprite.spritecollide(bottom_border, obstacles, True)
+    for col in collisions:
+        obstacles.remove(col)
+    for obstacle in obstacles:
+        obstacle.y += 1
+        obstacle.set_position()
+    while len(obstacles) < obstacle_count:
+        obstacles.add(Obstacle((52, 220, 60), 40, 40, None, 1))
+    return obstacles
+
+
+bottom_border = Obstacle(PINK, WIDTH, 2, 0, HEIGHT - 2)
 
 all_sprites = pygame.sprite.Group()
 
@@ -46,7 +83,14 @@ while not quit:
     for event in pygame.event.get():
         quit = event.type == pygame.QUIT
 
-    screen.fill(black)
+    screen.fill(BLACK)
+
     for sprite in all_sprites:
         screen.blit(sprite.image, sprite.rect)
+
+    delta_time = clock.tick(60)
+    obstacles = tick_obstacles(delta_time, obstacles, 11, bottom_border)
+    all_sprites.empty()
+    all_sprites.add(player)
+    all_sprites.add(*obstacles)
     pygame.display.flip()
